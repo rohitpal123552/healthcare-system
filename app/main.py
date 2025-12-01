b59f75d1ef3ab9da2326da7f21a65165911a23cd
rohitpal123552 <51694429+rohitpal123552@users.noreply.github.com>
2025-12-01 15:09:40 +0530

    Add database initialization on startup event
    Initialize PostgreSQL and MongoDB on startup.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from app.database.mongo import init_mongo
+from app.database.postgres import init_postgres
+
+# Import router
 from app.routers import patients, doctors, appointments, medical_records, prescriptions, billing, clinical_notes, imaging
-from app.seed.seed_router import router as seed_router
+# from app.seed.seed_router import router as seed_router
 
 app = FastAPI(title="Healthcare Hybrid DB API", version="1.0")
+
+
+@app.on_event("startup")
+def startup_event():
+    init_postgres() # Create all SQL Tables
+    init_mongo() # CREATE MONGO COLLECTION
+    print("Application startup completed")
+
 
 # include routers (ensure import paths are correct)
 app.include_router(patients.router)
@@ -13,4 +25,4 @@ app.include_router(prescriptions.router)
 app.include_router(billing.router)
 app.include_router(clinical_notes.router)
 app.include_router(imaging.router)
-app.include_router(seed_router)
+# app.include_router(seed_router)
